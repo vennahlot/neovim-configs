6ecf263db4ac42a5490719f4ef3c5675bccff0e1
vennahlot <vennahlot@users.noreply.github.com>
2023-06-20 16:44:16 -0700

    add abs path to inactive lualine

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -58,7 +58,11 @@ lualine.setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {{
+      'filename',
+      path = 2,  -- 0 = just filename, 1 = relative path, 2 = absolute path
+      file_status = true,  -- displays file status (readonly status, modified status)
+    }},
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}
